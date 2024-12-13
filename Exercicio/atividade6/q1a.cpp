0cb40aa085f6c8bd8db3e039d909562220df2b2a
StepGalvaoCourses <sgalvao@ifpi.edu.br>
2024-12-13 19:15:00 -0300

    Lista de Array

---
+++ Exercicio/atividade6/q1a.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(){
+    float notas[4];
+    notas[0] = 9.5;
+    notas[1] = 7.5;
+    notas[2] = 6.8;
+    notas[3] = 8.7;
+
+    float notas_2[] = {9.5, 7.5, 6.8, 8.7};
+}
