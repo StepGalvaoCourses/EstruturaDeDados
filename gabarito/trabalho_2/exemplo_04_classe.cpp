13e32ba78ab9d1c4fd7887b0cf0ea98dbdbcd8c5
StepGalvaoCourses <sgalvao@ifpi.edu.br>
2026-01-19 20:17:56 -0300

    Atualizando o trabalho

--- gabarito/trabalho_2/exemplo_04_classe.cpp
+++ gabarito/trabalho_2/exemplo_04_classe.cpp
@@ -12,14 +12,25 @@ typedef struct tipo_livro Livro;
 
 int main(){
     Livro biblioteca[2];
-    for(int i =0; i<2; i++){
-        printf("Digite o ISBN:");
-        scanf("%d",&biblioteca[i].ISBN);
-        printf("Digite o título:");
-        scanf("%s",biblioteca[i].titulo);
-        printf("Digite o número de páginas:");
-        scanf("%d",&biblioteca[i].numero_paginas);
-    }
+    
+    int indice = 0;
+    
+    printf("Digite o ISBN:");
+    scanf("%d",&biblioteca[indice].ISBN);
+    printf("Digite o título:");
+    scanf("%s",biblioteca[indice].titulo);
+    printf("Digite o número de páginas:");
+    scanf("%d",&biblioteca[indice].numero_paginas);
+    indice++;
+    
+    
+    printf("Digite o ISBN:");
+    scanf("%d",&biblioteca[indice].ISBN);
+    printf("Digite o título:");
+    scanf("%s",biblioteca[indice].titulo);
+    printf("Digite o número de páginas:");
+    scanf("%d",&biblioteca[indice].numero_paginas);
+
 
     for(int i =0; i<2;i++){
         printf("++++++++++++++++++++++Livro+++++++++++++++\n");
